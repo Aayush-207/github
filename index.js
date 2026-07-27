727605ba3599f5001272d2e64eac9397e00fe59c
Aayush-207 <aayushraut2006@gmail.com>
2026-07-27 10:19:16 +0530

    date chnages

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
-import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+// CONFIG: set your target date (YYYY-MM-DD) and number of commits
+const TARGET_DATE = "2024-01-15";
+const NUM_COMMITS = 5;
 
-  const data = {
-    date: date,
-  };
+const makeCommitsOnDate = (n) => {
+  if (n === 0) return simpleGit().push();
+
+  const date = moment(TARGET_DATE).format();
+  const data = { date };
+
+  console.log(`Making commit ${NUM_COMMITS - n + 1}/${NUM_COMMITS} on ${date}`);
 
   jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
+    simpleGit()
+      .add([path])
+      .commit(date, { "--date": date }, makeCommitsOnDate.bind(this, --n));
   });
 };
 
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
-
-  const data = {
-    date: date,
-  };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
-  });
-};
-
-makeCommits(10);
+makeCommitsOnDate(NUM_COMMITS);
